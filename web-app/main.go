90dd420340a61b1c1cba6a36f690a580223aea65
Mark Brown <mark.brown8790@gmail.com>
2024-12-08 23:45:26 +0000

    feat: added helm install back in

--- web-app/main.go
+++ web-app/main.go
@@ -18,11 +18,11 @@ func main() {
 		if r.Method == http.MethodPost {
 			action := r.FormValue("action")
 			if action == "Deploy Git" {
-				//returnUrl := helpers.DeployGit()
+				returnUrl := helpers.DeployGit()
 				client := helpers.NewKubeClient()
 				client.Apply("manifest-files/gitea.middleware.yaml")
 				client.Apply("manifest-files/gitea.ingress-route.yaml")
-				//message = "Deploy Git button pressed. Visit: " + returnUrl
+				message = "Deploy Git button pressed. Visit: " + returnUrl
 			} else if action == "Deploy Webtop" {
 				helpers.DeployWebtop()
 				message = "Deploy Webtop button pressed"
